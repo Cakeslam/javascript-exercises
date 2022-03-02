140e4b2c82c837750400dea4fbb0814c0580dda2
Matt Ward <mterryward@gmail.com>
2022-03-02 19:26:57 +0000

    Started exercise 3.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reversedString = splitString.reverse();
+    return reversedString.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray, num) {
+    for (const numb of myArray) {
+        if (myArray[numb] == num) {
+            myArray.splice(numb, 1);
+        }
+    }
+    return myArray;
 };
 
 // Do not edit below this line
